19e35dbf105cc4025aa2429f33974c756dd65e36
Arianna Billeci <176613378+aribi-95@users.noreply.github.com>
2025-10-21 19:36:47 +0200

    Fix per deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 // Connessione a MongoDB
